d5c0eb192212f0e6f0a2c52b91a60591ca011622
DreamingRaven <GeorgeRavenNR@outlook.com>
2020-01-31 22:09:27 +0000

    Added first freecad test for business card
    Trying to see if i can smoothly use freecad from pure python so that I
    can generate models without having to use the freecad editor.

---
+++ models/business_card.py
@@ -0,0 +1,14 @@
+import sys
+# append the freecad libraries to PATH so we can use them
+sys.path.append('/usr/lib/freecad/lib')
+# sys.path.append('/usr/lib/freecad-daily/lib')
+
+
+def main():
+    import FreeCAD as freecad
+    import Part as part
+    import Sketcher as sketcher
+
+
+if __name__ == "__main__":
+    main()
